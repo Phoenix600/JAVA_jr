fb0872a0eeb542d2bcfda20830ebed71fcae24ce
Phoenix600 <pranayramteke613@gmail.com>
2022-09-17 00:43:49 -0700

    New files has been added

---
+++ Chapter-01/03_basic_data_type.java
@@ -0,0 +1,28 @@
+// Import java libraries here 
+import java.util.Scanner;
+import java.lang.String;
+
+public class DataTypes
+{
+  public static void main(String[] args)
+  {
+    // Declare variables here 
+    double myDouble = 3.33333;
+    float myFloat = 3.3F;
+
+    double myDoubleScientific = 3.44e4;
+
+    System.out.println("The value of myDouble variable is : " + myDouble);
+    System.out.println("The value of myFloat variable is : " + myFloat);
+    System.out.println("The value of scientif variable is : " + myDoubleScientific);
+
+    
+    // char literals 
+    char letter = 'S';
+    String name = "Sheldon Cooper";
+    
+    System.out.printf("%s for %s \n",letter,name);
+
+
+  }
+}
